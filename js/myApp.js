e1bc9f3ff4e2d607e3dd2ecf3bb7badc9a623293
JohnWangJNZ <373437940@qq.com>
2016-06-20 11:55:41 +1200

    Update myApp.js

--- js/myApp.js
+++ js/myApp.js
@@ -1,62 +1,28 @@
 window.onload = function () {
-
-
     myApp();
 
 }
 function myApp() {
 
-    var theCanvas = document.getElementById("theCanvas");
+    var theCanvas = document.getElementById("theCanvas");//the drawArea canvas
     var context = theCanvas.getContext("2d");
-    var words = new Array();
+    //  var words = new Array();
     var imgContainer = document.getElementById("TMImages");
-    var TMimages = imgContainer.getElementsByTagName("img");
-    var TMData = [];
+    var TMimages = imgContainer.getElementsByTagName("img");//get the template images
 
-    var TMtimer;
+    var keyDown = false;
 
-    var drawImg;
+    var TMtimer;// a timer to set the span of individual image shown in TM process
+    var drawImg;// to store the handwritten character image
 
-    //create Template Maching Dataset
-    for (var i = 0; i < TMimages.length; i++) {
-        context.drawImage(TMimages[i], 0, 0, TMimages[i].width, TMimages[i].height);
-        var imgdata = context.getImageData(0, 0, TMimages[i].width, TMimages[i].height);
-        var newImage = imgdata;
-        // console.log(newImage);
-        TMData.push(newImage);
-    }
 
-    var img = document.getElementById("myImage");
-    //var wordSource = new Array();
     drawScreen();
     var btnCreate = document.getElementById("btn_create");
     var btnReset = document.getElementById("reset");
     btnReset.addEventListener('click', function () {
         window.location.reload();
     }, false);
-    //btnCreate.addEventListener("click", createTM, false);
-    //function createTM() {
-    //    //var sur = theCanvas.toDataURL();
-    //    //img.src = sur;
-    //    var img1 = TMData[0];
-    //    for (var i = 0; i < img1.data.length - 4; i += 4) {
-    //        //img1.data[i] = 255;
-    //        //img1.data[i + 1] = 0;
-    //        //img1.data[i + 2] = 0;
-    //        if (img1.data[i] < 100 && img1.data[i + 1] < 100 && img1.data[i + 2] < 100) {
-    //            img1.data[i] = 255;
-    //            img1.data[i + 1] = 255;
-    //            img1.data[i + 2] = 255;
-    //        }
-    //        else {
-    //            img1.data[i] = 0;
-    //            img1.data[i + 1] = 0;
-    //            img1.data[i + 2] = 0;
-    //        }
 
-    //    }
-    //    context.putImageData(img1, 0, 0);
-    //}
 
     function drawScreen() {
 
@@ -64,7 +30,6 @@ function myApp() {
         context.fillRect(0, 0, theCanvas.width, theCanvas.height);
         context.strokeStyle = "#f2c3a1";
         context.strokeRect(2, 2, theCanvas.width - 2, theCanvas.height - 2);
-
 
         context.beginPath();
         context.setLineDash([5]);
@@ -84,11 +49,9 @@ function myApp() {
 
     }
 
-    var keyDown = false;
 
+    // draw the character on the canvas
     theCanvas.onmousedown = function (e) {
-        //console.log(e.offsetX);//offsetX...
-        //console.log(e.offsetY);
 
         keyDown = true;
         var newWord = new Array();
@@ -96,13 +59,13 @@ function myApp() {
             if (keyDown) {
                 var positionX = e.offsetX;
                 var positionY = e.offsetY;
-                console.log("x:" + positionX + "y:" + positionY);
+                // console.log("x:" + positionX + "y:" + positionY);
                 newWord.push({ x: positionX, y: positionY });
                 drawCharacter();
             }
             function drawCharacter() {
                 for (var i = 0; i < newWord.length - 3; i++) {
-                    console.log(newWord[i].x);
+                    // console.log(newWord[i].x);
                     context.save();
                     context.beginPath();
                     context.strokeStyle = "black";
@@ -134,44 +97,38 @@ function myApp() {
         displayResult();
     }
     function displayResult() {
-        // var chart = document.getElementById('result');
-        // chart.innerHTML = "The recognized number: 2";
+
         var resultCanvas = document.getElementById('resultCanvas');
         var context2 = resultCanvas.getContext('2d');
 
-        context2.fillStyle = "green";
-        context2.fillRect(20, 0, 20, 300);
-        context2.fillRect(60, 40, 20, 300);
-        context2.fillRect(100, 80, 20, 300);
-        context2.fillRect(140, 120, 20, 300);
-        context2.fillRect(180, 160, 20, 300);
-        context2.fillRect(220, 220, 20, 300);
-        context2.fillRect(260, 160, 20, 300);
-        context2.fillRect(300, 110, 20, 300);
-        context2.fillRect(340, 20, 20, 300);
-        context2.fillRect(380, 40, 20, 300);
+        context2.fillStyle = "#ffffff";
+        context2.fillRect(0, 0, resultCanvas.width, resultCanvas.height);
+        setTimeout(function () {
+            context2.fillStyle = "green";
+            context2.fillRect(20, 0, 20, 300);
+            context2.fillRect(60, 40, 20, 300);
+            context2.fillRect(100, 80, 20, 300);
+            context2.fillRect(140, 120, 20, 300);
+            context2.fillRect(180, 160, 20, 300);
+            context2.fillRect(220, 220, 20, 300);
+            context2.fillRect(260, 160, 20, 300);
+            context2.fillRect(300, 110, 20, 300);
+            context2.fillRect(340, 20, 20, 300);
+            context2.fillRect(380, 40, 20, 300);
+        }, 200);
+
 
     }
 
+    /*The following snippets using jquery*/
+
     $('#myModal').on('shown.bs.modal', function (e) {
-
-
 
 
         displayProcess(TMimages.length);
         var $drawImage = $('#drawImg');
         $drawImage.append('<img src="' + drawImg.src + '" width="150" height="150">');
-        //for (var i = 0; i < TMimages.length; i++) {
 
-        //    var tmImg=$(TMimages[i]);
-        //    $('#TMimage').attr("src", tmImg.attr("src"));
-        //    currentValue += 10;
-        //    $progressBar.attr("aria-valuenow", currentValue);
-        //    $progressBar.attr("style", "width:" + currentValue + "%");
-        //    $progressBar.text(currentValue + "%");
-
-        //    console.log(currentValue);
-        //}
 
     });
     function displayProcess(times) {
@@ -194,3 +151,4 @@ function myApp() {
     }
 
 }
+
